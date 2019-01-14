b7ebd90a8e6a050adb85d2841a4aaf03e8a2feba
Marco Mehrhoff <marcomehrhoff@gmail.com>
2019-01-14 15:24:46 +0100

    fixed issues with start.sh; renamed to control.sh

---
+++ control.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+ADMIN_USER=admin
+ADMIN_PASSWORD=admin
+
+COLLECT_HOST_NETWORK=true
+IP_ADDRESS=192.168.162.60
+
+if [ $COLLECT_HOST_NETWORK ]; then
+	DOCKER_FILE=./.tmp/docker-compose.yml
+else
+	DOCKER_FILE=./docker-compose.yml
+fi
+
+case "$1" in
+	start)
+		if [ $COLLECT_HOST_NETWORK ]; then
+			if [ "$IP_ADDRESS" = "" ] ; then
+				echo "IP address of host not configured"
+				exit 1
+			fi
+
+			mkdir -p .tmp/prometheus
+			# copy used configurations into .tmp
+			cp -r ./prometheus/* .tmp/prometheus/
+
+			# configuring prometheus to scrape from external network
+			sed -i "s/nodeexporter:/$IP_ADDRESS:/g" .tmp/prometheus/prometheus.yml
+			sed -i "s/cadvisor:/$IP_ADDRESS:/g" .tmp/prometheus/prometheus.yml
+
+			# 
+			cp docker-compose.yml .tmp/docker-compose.yml
+
+			# replace mapping paths
+			sed -i 's/- \.\/alertmanager\//- \.\.\/alertmanager\//g' .tmp/docker-compose.yml
+			sed -i 's/- \.\/caddy\//- \.\.\/caddy\//g' .tmp/docker-compose.yml
+			sed -i 's/- \.\/grafana\//- \.\.\/grafana\//g' .tmp/docker-compose.yml
+
+			docker-compose -f $DOCKER_FILE up -d
+			docker-compose -f docker-compose.exporters.yml up -d
+		else
+			docker-compose -f $DOCKER_FILE up -d
+		fi
+		;;
+	stop)
+		docker-compose -f $DOCKER_FILE down
+		;;
+	restart)
+		docker-compose -f $DOCKER_FILE restart
+		;;
+	logs)
+		docker-compose -f $DOCKER_FILE logs
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart|logs}"
+		exit 1
+esac
